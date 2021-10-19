c3ee508a979524360ef5da82edc3953ba562d543
Dery Sudrajat <dery.sudrajat17@gmail.com>
2021-10-19 09:01:18 +0700

    add Conditional Statement, Control Transfer, Guard, and Sample Project

---
+++ ControlFlow.playground/Pages/Control Transfer.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+import Foundation
+import Cocoa
+
+// Continue
+let puzzleInput = "great minds think alike"
+var puzzleOutput = ""
+let charactersToRemove: [Character] = ["a", "e", "i", "o", "u", " "]
+for character in puzzleInput {
+   if charactersToRemove.contains(character) {
+       continue
+   }
+   puzzleOutput.append(character)
+}
+print(puzzleOutput)
+
+// Break
+let numberSymbol: Character = "三"  // Chinese symbol for the number 3
+var possibleIntegerValue: Int?
+switch numberSymbol {
+case "1", "١", "一", "๑":
+   possibleIntegerValue = 1
+case "2", "٢", "二", "๒":
+   possibleIntegerValue = 2
+case "3", "٣", "三", "๓":
+   possibleIntegerValue = 3
+case "4", "٤", "四", "๔":
+   possibleIntegerValue = 4
+default:
+   break
+}
+if let integerValue = possibleIntegerValue {
+   print("The integer value of \(numberSymbol) is \(integerValue).")
+} else {
+   print("An integer value could not be found for \(numberSymbol).")
+}
+
+// Fallthrough
+// Continue in swich case checking
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+case 2, 3, 5, 7, 11, 13, 17, 19:
+   description += " a prime number, and also"
+   fallthrough
+default:
+   description += " an integer."
+}
+print(description)
